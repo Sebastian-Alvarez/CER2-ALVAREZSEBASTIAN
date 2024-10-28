7aeedce172b64ae44844f2800c8028c1cc59afcc
Sebastian-Alvarez <sebastian.alvarezav@usm.cl>
2024-10-28 00:38:02 -0300

    Model Producto created

--- TiendaVerde/Inventario/models.py
+++ TiendaVerde/Inventario/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=100)
+    precio = models.FloatField()
+    cantidad = models.IntegerField()
+
+    def __str__(self):
+        return self.name
