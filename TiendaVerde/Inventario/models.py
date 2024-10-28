0abdd0f27f10c6e480ac117044af42fff0b0fe70
Sebastian-Alvarez <sebastian.alvarezav@usm.cl>
2024-10-28 10:57:21 -0300

    images treatment

--- TiendaVerde/Inventario/models.py
+++ TiendaVerde/Inventario/models.py
@@ -7,6 +7,7 @@ class Producto(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     precio = models.FloatField()
+    img = models.ImageField(null=True, blank=True)
     descripcion = models.TextField(null=True, blank=True)
 
     def __str__(self):
